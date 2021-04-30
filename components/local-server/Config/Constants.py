596886eccfb1274145f05f5310beec1ec0770a4c
Linnea Gustavsen <71972850+linneagustavsen@users.noreply.github.com>
2021-04-30 13:15:22 +0200

    Changed invalid username or password to user credentials

--- components/local-server/Config/Constants.py
+++ components/local-server/Config/Constants.py
@@ -4,7 +4,7 @@ from enum import Enum
 
 class ERROR_CODES(Enum):
     SESSION_EXISTS = 0
-    INVALID_USERNAME_OR_PASSWORD = 1
+    INVALID_USER_CRECENTIALS = 1
     UNKNOWN_MESSAGE_TYPE = 2
     WALKIE_IN_USE = 3
     USER_ALREADY_LOGGED_IN = 4
@@ -14,9 +14,8 @@ class ERROR_CODES(Enum):
     INVALID_TOKEN = 8
     NOT_PARTICIPATING_IN_CHANNEL = 9
     ACCESS_DENIED = 10
-    #INVALID_SERVER = 11
-    #INVALID_WALKIE = 12
-    #Er dette greit??? Man sjekker om authentication server har den serveren eller walkien som prøver å logge inn en bruker er i databasen
+    INVALID_SERVER = 11
+    INVALID_WALKIE = 12
 
 
 class AUTH_SERVER_MESSAGE(Enum):
