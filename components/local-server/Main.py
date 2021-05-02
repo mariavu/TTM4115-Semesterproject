b393fa7ee740af2ed1cae5dd2b8265e84f9fc622
Henrik Udnes <henrik@udnes.dev>
2021-05-02 16:06:48 +0200

    Removed unnecessary infinite loop.

--- components/local-server/Main.py
+++ components/local-server/Main.py
@@ -1,6 +1,5 @@
 from Config.Mqtt import *
 from Config.Seed import seedDatabase
-from time import sleep
 
 from Infrastructure.Controller import Controller
 from Infrastructure.Database import Database
@@ -14,9 +13,6 @@ def main():
 
     controller = Controller(db, MQTT_BROKER, MQTT_PORT, "1", MQTT_ROOT_TOPIC, tpm)
     controller.start()
-
-    while 1:
-        sleep(100)
 
 
     #Composition root, here we load all necessarry configurations and bootup the controller and ensure that the database is seeded.
